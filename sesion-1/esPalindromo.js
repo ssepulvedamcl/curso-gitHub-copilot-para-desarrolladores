0bd4482a804054f6dbc04dfb363e512fbe27f76a
Sergio Sepúlveda Montealegre <sergiosepulveda.montealegre@gmail.com>
2025-08-14 10:58:42 -0400

    Add palindrome checking algorithm

---
+++ sesion-1/esPalindromo.js
@@ -0,0 +1,9 @@
+//generame un algoritmo para saber si una palabra es palimdrome o no
+function esPalindromo(palabra) {
+    // Eliminar espacios y convertir a minúsculas
+    palabra = palabra.replace(/\s+/g, '').toLowerCase();
+    // Invertir la palabra
+    var palabraInvertida = palabra.split('').reverse().join('');
+    // Comparar la palabra original con la invertida
+    return palabra === palabraInvertida;
+}
